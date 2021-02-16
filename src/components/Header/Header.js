aef7e072faa1cd74110b563538713bc6b7887aa3
Vladimir <vladgoloyan@gmail.com>
2021-02-16 21:05:50 +0400

    fisrt commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div className="app-header">
+            <nav>
+                <ul>
+                    <li>posts</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
